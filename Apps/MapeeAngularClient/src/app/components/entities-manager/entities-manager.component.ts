72f8c935e535fd70370da01974d4f67a836d8313
CarolinaKL <carolina.kats84@gmail.com>
2022-07-12 11:50:55 +0300

    added entities component manager support for http and move entity

--- Apps/MapeeAngularClient/src/app/components/entities-manager/entities-manager.component.ts
+++ Apps/MapeeAngularClient/src/app/components/entities-manager/entities-manager.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MapEntitiesService } from 'src/app/services/mapEntities/map-entities.service';
 import { MissionMapService } from 'src/app/services/missionMap/mission-map.service';
 import { IMapEntity } from 'src/app/models/mapEntityModels/iMap-entity.model';
@@ -13,15 +13,15 @@ import { MapModel } from 'src/app/models/mapModel/mapModel';
   styleUrls: ['./entities-manager.component.css']
 })
 export class EntitiesManagerComponent implements OnInit, AfterViewInit {
-  
+
   public map: IMapModel;
 
   public entityImageSrc: string;
 
   public entityImageElement: ElementRef;
 
-  private _canvas!: HTMLCanvasElement;  
-  
+  private _canvas!: HTMLCanvasElement;
+
   @ViewChild('canvas')
   private _canvasRef: ElementRef = {} as ElementRef;
 
@@ -30,30 +30,30 @@ export class EntitiesManagerComponent implements OnInit, AfterViewInit {
     x: 0,
     y: 0
   });
-  
-  constructor(private formBuilder: FormBuilder, private mapEntitiesService: MapEntitiesService, 
-                                   private missionMapService: MissionMapService) {
+
+  constructor(private formBuilder: FormBuilder, private mapEntitiesService: MapEntitiesService,
+    private missionMapService: MissionMapService) {
     this.map = new MapModel("empty", "");
     this.entityImageSrc = "../../../assets/location.png"
-    this.entityImageElement = new ElementRef(document.getElementById("entityImage"));                                
+    this.entityImageElement = new ElementRef(document.getElementById("entityImage"));
   }
 
   ngOnInit(): void {
     let missionMapObserver = this.missionMapService.missionMapChanged;
-    missionMapObserver.subscribe( (mapName) => {
+    missionMapObserver.subscribe((mapName) => {
       let currentMap = MapDummyData.find((map) => map.name == mapName);
       if (currentMap == undefined) {
-        this.map = new MapModel("empty", "");  
+        this.map = new MapModel("empty", "");
       } else {
         this.map = currentMap;
       }
-    } )
+    })
   }
 
   ngAfterViewInit(): void {
     this.entityImageElement.nativeElement = document.getElementById("mapEntity");
     // this._canvas = this._canvasRef.nativeElement;
-    // this.subscribeToFormChanges();
+    this.subscribeToFormChanges();
     // this.subscribeToCanvasClicks();
 
     // let context = this._canvas.getContext("2d");
@@ -69,8 +69,8 @@ export class EntitiesManagerComponent implements OnInit, AfterViewInit {
     let name = this.mapEntityForm.value.name as string;
     let x = this.mapEntityForm.value.x as number;
     let y = this.mapEntityForm.value.y as number;
-    let entity = new MapEntity(name, x, y)  
-    this.mapEntitiesService.addMapEntity(entity);
+    let entity = new MapEntity(name, x, y)
+    this.mapEntitiesService.addMapEntity(entity).subscribe();
     this.mapEntityForm.reset();
   }
 
@@ -81,27 +81,26 @@ export class EntitiesManagerComponent implements OnInit, AfterViewInit {
       this.moveIcon(values.x, values.y);
     })
 
-    this.mapEntityForm.controls.x.registerOnChange(()=> {
-      // this.moveIcon(this.mapEntityForm.controls.x.v, values.y);
-    })
+    // this.mapEntityForm.controls.x.registerOnChange(()=> {
+    //   // this.moveIcon(this.mapEntityForm.controls.x.v, values.y);
+    // })
   }
 
-  public onClick(e: MouseEvent ) {
-    this.mapEntityForm.controls.x.setValue(e.offsetX) ;
+  public onClick(e: MouseEvent) {
+    this.mapEntityForm.controls.x.setValue(e.offsetX);
     this.mapEntityForm.controls.y.setValue(e.offsetY);
-  
+
     // this.entityImageElement.nativeElement.style.transform = `translate(${e.offsetX - 50}px, ${e.offsetY}px)`
     this.moveIcon(e.offsetX - 50, e.offsetY);
   }
 
-  private subscribeToCanvasClicks()
-  {
+  private subscribeToCanvasClicks() {
     this._canvas.addEventListener('click', (clickEvent) => {
       this.mapEntityForm.value.x = clickEvent.offsetX;
-      this.mapEntityForm.value.y = clickEvent.offsetY;  
+      this.mapEntityForm.value.y = clickEvent.offsetY;
 
       this.moveIcon(clickEvent.offsetX, clickEvent.offsetY);
-    })   
+    })
   }
 
   private moveIcon(newX: number, newY: number) {
